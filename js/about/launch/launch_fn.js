a53f07fbeb3c51999127b28b7e108236cc4757b9
PayBas <contact@paybas.com>
2014-10-29 00:45:30 +0100

    Hard-code countdown-download buttons

--- js/about/launch/launch_fn.js
+++ js/about/launch/launch_fn.js
@@ -82,18 +82,4 @@ $(function() {
 	});
 
 	$navContainer.waypoint('sticky');
-
-	// COUNTDOWN -------------------------------------------------------------
-
-	var $timer = $('#countdown-big');
-
-	var utc = new Date('Tue Oct 28 2014 17:00:00 GMT+0000 (UTC)').getTime();
-	$timer.countdown(utc, function(event) {
-		$(this).html(event.strftime('<div class="timer-unit days"><span class="timer-big">%D</span><span class="timer-small">days</span></div><div class="timer-unit hours"><span class="timer-big">%H</span><span class="timer-small">hours</span></div><div class="timer-unit minutes"><span class="timer-big">%M</span><span class="timer-small">minutes</span></div><div class="timer-unit seconds"><span class="timer-big">%S</span><span class="timer-small">seconds</span></div>'));
-	}).on('finish.countdown', function() {
-		$('.lead-masthead').find('.countdown-big').remove();
-		$('.lead-masthead').append('<p class="lead-sub-buttons lead-sub-title text-center"><a class="lead-clear-outline download-btn" href="https://www.phpbb.com/downloads/" style="">Download 3.1</a> <span>or</span> <a class="lead-clear-outline test-btn" href="http://www.try-phpbb.com/31x/" style="">Try It</a></p>');
-	});
-
-	$timer.show();
 });
