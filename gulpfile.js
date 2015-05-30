08754776092c5873a90a6c67a315c30bfb42b771
Callum Macrae <callum@macr.ae>
2015-05-30 10:56:39 +0200

    sprited some images and broke the rest

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
 var config = {
-	BUILD_DEST: 'build'
+	BUILD_DEST: 'build',
+	MAX_SPRITE_SIZE: 50 * 1024 // 50kb
 };
 
 gulp.task('css', function () {
 	var processors = [
 		require('postcss-import')(),
-		require("postcss-url")({
+		require('postcss-sprites')({
+			spriteName: 'build/sprite.png'
+		}),
+		require('postcss-url')({
 			url: function (url) {
 				if (url.indexOf('/') === 0 ||
 					url.indexOf('data:') === 0 ||
